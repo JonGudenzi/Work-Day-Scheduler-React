464b8d1035db820260c478734d4db100bf749386
JonGudenzi <jongudenzi@gmail.com>
2021-03-06 14:37:05 -0500

    Create state for each task hour

--- work-day-scheduler/src/App.js
+++ work-day-scheduler/src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import HourRow from './HourRow';
 import Header from './Header';
 
-function App() {
-  const hours = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5]
+class App extends React.Component{
+  state = {
+    hourTask: {
+      8: "",
+      9: "",
+      10: "",
+      11: "",
+      12: "",
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: ""
+    }
+  };
+
+  render(){
+    const hours = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5]
   return (
     <div className="App">
       {hours.map((hour)=>{
-        return <HourRow key={hour.toString()} currentHour={hour} />
+        return <HourRow currentHour={hour} 
+        stateHour={state.hourTask[hour]}/>
       })}
     </div>
   );
+}
 }
 
 export default App;
--- work-day-scheduler/src/HourRow.jsx
+++ work-day-scheduler/src/HourRow.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+// import { useState } from 'react'
+
 
 
 function HourRow(props) {
+
   const saveBtn = () => {
     var userText = document.getElementById(`${props.currentHour}-hour-task`).value;
-  
+    var stateHour = document.getElementById(props.stateHour).value;
     var hourTask = document.getElementById(props.currentHour);
    
-  localStorage.setItem(hourTask, userText);
+  localStorage.setItem(hourTask, userText, stateHour);
   
   }
     return (
       <div className="row" id={props.currentHour}>
           <label className="col-sm-1 hour">{props.currentHour}</label>
-          <textarea id={`${props.currentHour}-hour-task`} type="text" className="col-md-10 time-block" ></textarea>
+          <textarea id={`${props.currentHour}-hour-task`} id={props.stateHour}  type="text" className="col-md-10 time-block" ></textarea>
           <button onClick={saveBtn} type="submit" className="saveBtn col-sm-1"><i className="fas fa-save save"></i></button>
       </div>
     );
