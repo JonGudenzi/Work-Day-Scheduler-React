6ebbb3278439382d57e9a3308ee76463acf9adf9
JonGudenzi <jongudenzi@gmail.com>
2021-03-06 18:11:14 -0500

    saving time slots to local storage as an object

--- work-day-scheduler/src/App.js
+++ work-day-scheduler/src/App.js
@@ -5,27 +5,28 @@ import HourRow from './HourRow';
 function App() {
 
 
-  const [hourTask, setHourTask] = useState([
+  const [hourTask, setHourTask] = useState(JSON.parse(localStorage.getItem("time"))||[
     {hour:8, text: "" },
     {hour:9, text: "" },
     {hour:10, text: "" },
     {hour:11, text: "" },
     {hour:12, text: "" },
-    {hour:1, text: "" },
-    {hour:2, text: "" },
-    {hour:3, text: "" },
-    {hour:4, text: "" },
-    {hour:5, text: "" }
+    {hour:13, text: "" },
+    {hour:14, text: "" },
+    {hour:15, text: "" },
+    {hour:16, text: "" },
+    {hour:17, text: "" }
   ])
   console.log(hourTask);
   return (
     <div className="App">
       {
         hourTask.map(({hour, text}) => {
-          console.log(hour);
+    
           return (
             <HourRow
               currentHour={hour}
+              text={text}
               key={hour}
               hourTask={hourTask}
               setHourTask={setHourTask} />
--- work-day-scheduler/src/HourRow.jsx
+++ work-day-scheduler/src/HourRow.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-// import { useState } from 'react'
+import { useState } from 'react'
 
 function HourRow(props) {
+  console.log(props.text)
+const [textVal, setTextVal] = useState(props.text)
 
   const saveBtn = () => {
-    console.log(props.hourTask.filter(hourTime => hourTime.hour === props.currentHour));
-    // props.setHourTask(hourTask.filter(hourTime => hourTime.hour === currentHour))
+    let newVal = props.hourTask.filter(hourTime => hourTime.hour === props.currentHour);
+    newVal[0].text = textVal;
+    const oldVals = props.hourTask.filter(hourTime => hourTime.hour !== props.currentHour);
+    const newArr = [...oldVals, ...newVal].sort((a, b) => a.hour - b.hour)
+    props.setHourTask(newArr)
+    localStorage.setItem("time", JSON.stringify(newArr));
   }
+  
   return (
     <div className="row" id={props.currentHour}>
       <label className="col-sm-1 hour">{props.currentHour}</label>
-      <textarea id={`${props.currentHour}-hour-task`} type="text" className="col-md-10 time-block"></textarea>
+      <textarea value={textVal} onChange={e => setTextVal(e.target.value)} id={`${props.currentHour}-hour-task`} type="text" className="col-md-10 time-block"></textarea>
       <button onClick={saveBtn} type="submit" className="saveBtn col-sm-1"><i className="fas fa-save save"></i></button>
     </div>
   );
