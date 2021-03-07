3577d3468720e947c119d31a49614eb7ce2658b5
JonGudenzi <jongudenzi@gmail.com>
2021-03-07 18:18:25 -0500

    correction of file structure

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Moment from "react-moment";
+
+
+function Header() {
+    return (
+        <header className="jumbotron">
+      <h1 className="display-3">Work Day Scheduler</h1>
+      <p className="lead">A simple calendar app for scheduling your work day</p>
+      <Moment format="dddd MMMM Do YYYY, h:mm a"></Moment>
+      <p className="lead"></p>
+    </header>
+    )
+}
+
+export default Header;
